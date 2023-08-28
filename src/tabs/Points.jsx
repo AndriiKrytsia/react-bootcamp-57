c1b0347d8e89cd7348878feb40ea4b5d84490e0f
Elonka5 <101475428+Elonka5@users.noreply.github.com>
2023-08-28 14:33:21 +0300

    add branch

--- src/tabs/Points.jsx
+++ src/tabs/Points.jsx
@@ -1,45 +1,45 @@
 import { PointsOptions } from 'components/Points/PointsOptions';
 import { Statistics } from 'components/Points/Statistics';
 import { Section } from 'components/Section/Section';
-import { Component } from 'react';
-export class Points extends Component {
-  state = {
+import { useState } from 'react';
+
+export const Points = () => {
+  const [points, setPoints] = useState({
     one: 0,
     two: 0,
     three: 0,
     four: 0,
     five: 0,
-  };
+  });
 
-  leavePoints = (option, value) => {
-    this.setState(prevState => ({
+  const leavePoints = (option, value) => {
+    setPoints(prevState => ({
+      ...prevState,
       [option]: prevState[option] + value,
     }));
   };
 
-  totalPoints = () => {
-    return Object.values(this.state).reduce((acc, option) => acc + option, 0);
+  const totalPoints = () => {
+    return Object.values(points).reduce((acc, option) => acc + option, 0);
   };
 
-  render() {
-    const total = this.totalPoints();
-    return (
-      <>
-        <Section title="Leave point">
-          <PointsOptions
-            options={Object.keys(this.state)}
-            onLeavePoint={this.leavePoints}
-          />
-        </Section>
+  const total = totalPoints();
+  return (
+    <>
+      <Section title="Leave point">
+        <PointsOptions
+          options={Object.keys(points)}
+          onLeavePoint={leavePoints}
+        />
+      </Section>
 
-        <Section title="Total points">
-          {total > 0 ? (
-            <Statistics total={total} options={Object.entries(this.state)} />
-          ) : (
-            <p>Not Points</p>
-          )}
-        </Section>
-      </>
-    );
-  }
-}
+      <Section title="Total points">
+        {total > 0 ? (
+          <Statistics total={total} options={Object.entries(points)} />
+        ) : (
+          <p>Not Points</p>
+        )}
+      </Section>
+    </>
+  );
+};
