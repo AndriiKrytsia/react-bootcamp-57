82d84dcfb2799a468271f7c76d99c67be33e8abc
DenShtephen <135002535+DenShtephen@users.noreply.github.com>
2023-08-30 14:37:33 +0300

    add Routes and Route

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,34 +1,20 @@
-import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
-import 'react-tabs/style/react-tabs.css';
-
-import { Points, Props, Counter, Todos, Gallery } from 'tabs';
+import { Route, Routes } from 'react-router-dom';
+import { Points, Props, Counter, Todos, Gallery, Home } from 'pages';
+import { Header } from './Header/Header';
+import { Cocktails } from 'pages/Cocktails';
 
 export const App = () => {
   return (
-    <Tabs>
-      <TabList>
-        <Tab>Gallery</Tab>
-        <Tab>Todos</Tab>
-        <Tab>Props</Tab>
-        <Tab>Points</Tab>
-        <Tab>Counter</Tab>
-      </TabList>
-
-      <TabPanel>
-        <Gallery />
-      </TabPanel>
-      <TabPanel>
-        <Todos />
-      </TabPanel>
-      <TabPanel>
-        <Props />
-      </TabPanel>
-      <TabPanel>
-        <Points />
-      </TabPanel>
-      <TabPanel>
-        <Counter />
-      </TabPanel>
-    </Tabs>
+    <Routes>
+      <Route path="/" element={<Header />}>
+        <Route index element={<Home />} />
+        <Route path="cocktails" element={<Cocktails />} />
+        <Route path="gallery" element={<Gallery />} />
+        <Route path="todos" element={<Todos />} />
+        <Route path="props" element={<Props />} />
+        <Route path="points" element={<Points />} />
+        <Route path="counter" element={<Counter />} />
+      </Route>
+    </Routes>
   );
 };
--- src/tabs/index.js
+++
@@ -1,5 +0,0 @@
-export * from 'tabs/Points';
-export * from 'tabs/Props';
-export * from 'tabs/Counter';
-export * from 'tabs/Todos';
-export * from 'tabs/Gallery';
