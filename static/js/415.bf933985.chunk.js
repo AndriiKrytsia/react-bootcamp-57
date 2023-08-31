b141afbb301afd15ed32553bf485b5506ec318b3
gostraa <134202645+gostraa@users.noreply.github.com>
2023-08-31 12:26:30 +0000

    Deploying to gh-pages from @ AndriiKrytsia/react-bootcamp-57@6bdf572c2105e10f22592dcc81d4bde88a90b097 🚀

---
+++ static/js/415.bf933985.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[415],{5415:function(e,t,r){r.r(t);var c=r(184);t.default=function(){return(0,c.jsx)("h1",{children:"Welcome to our site"})}}}]);
+//# sourceMappingURL=415.bf933985.chunk.js.map
